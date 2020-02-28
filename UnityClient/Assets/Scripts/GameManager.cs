52f04b43e66d400ba829f6d8d7aa138360ba0dac
Jason <jason_skillman@outlook.com>
2020-02-28 18:29:55 -0500

    Created GameManager script and prefab

---
+++ UnityClient/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    void Awake() {
+        NetworkManager.GetInstance().StartServer();
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            //Debug.Log("Space");
+        }
+    }
+
+}
