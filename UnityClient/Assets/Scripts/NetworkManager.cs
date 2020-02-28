a4e962eeafd33c02e49531a350850fb1195db30d
Jason <jason_skillman@outlook.com>
2020-02-28 16:53:23 -0500

    Created NetworkManager script and prefab

---
+++ UnityClient/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using SocketIO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    private static SocketIOComponent socket;
+
+
+    public bool HasServerStarted {
+        get;
+        private set;
+    }
+
+
+    void Awake() {
+        socket = GetComponent<SocketIOComponent>();
+    }
+
+    void Start() {
+        
+    }
+
+    public void StartServer() {
+        HasServerStarted = true;
+    }
+
+}
