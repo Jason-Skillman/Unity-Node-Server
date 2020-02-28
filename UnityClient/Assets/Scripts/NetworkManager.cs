d90a6ba47492df1f0c34779f931baffcdfc96952
Jason <jason_skillman@outlook.com>
2020-02-28 18:30:12 -0500

    Setup basic socket.io connection

--- UnityClient/Assets/Scripts/NetworkManager.cs
+++ UnityClient/Assets/Scripts/NetworkManager.cs
@@ -5,25 +5,59 @@ using UnityEngine;
 
 public class NetworkManager : MonoBehaviour {
 
+    private static NetworkManager INSTANCE;
+
     private static SocketIOComponent socket;
 
 
-    public bool HasServerStarted {
+    public bool HasConnected {
         get;
         private set;
     }
 
 
+    private NetworkManager() { }
+
+
     void Awake() {
+        if(INSTANCE == null) INSTANCE = this;
+
         socket = GetComponent<SocketIOComponent>();
     }
 
     void Start() {
-        
+        socket.On("open", OnConnected);
     }
 
+    /// <summary>
+    /// Called when this client first connects to the server
+    /// </summary>
+    /// <param name="obj"></param>
+    private void OnConnected(SocketIOEvent obj) {
+        Debug.Log("Client connected to server");
+    }
+
+    /// <summary>
+    /// Returns the singleton instance
+    /// </summary>
+    /// <returns></returns>
+    public static NetworkManager GetInstance() {
+        if(INSTANCE == null) INSTANCE = new NetworkManager();
+        return INSTANCE;
+    }
+
+    /// <summary>
+    /// Starts the socket.io connection with the server
+    /// </summary>
     public void StartServer() {
-        HasServerStarted = true;
+        //Has the connection already been started
+        if(HasConnected) {
+            Debug.Log("Server connection already started");
+            return;
+        }
+
+        HasConnected = true;
+        socket.Connect();
     }
 
 }
