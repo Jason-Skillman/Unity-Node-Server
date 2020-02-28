f1b0865a925a88aa0f138854c74e84e7b8e7ba60
Jason <jason_skillman@outlook.com>
2020-02-28 18:56:09 -0500

    Server keeps track of online clients

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -1,9 +1,24 @@
 var io = require('socket.io')(process.env.PORT || 3000)
 var shortid = require('shortid')
 
+var clients = []
+
 //Called when a new client has connected
 io.on('connection', function(socket) {
     console.log('Client connected')
+
+    //Create a unique generated id for the new client
+    var clientId = shortid.generate()
+    clients.push(clientId)
+    console.log('Clients ' + clients.length)
+
+
+    //Called when this client has disconnected from the server
+    socket.on('disconnect', function() {
+        console.log('Client disconnected')
+        clients.pop(clientId)
+        console.log('Clients ' + clients.length)
+    })
 })
 
 console.log('Server connected')
